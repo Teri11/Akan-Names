55bba955bf8f1a900e2f190b8f25f66b518516dd
Teri11 <augustineteri157@gmail.com>
2021-12-12 20:18:56 +0300

    added js script

---
+++ main.js
@@ -0,0 +1,41 @@
+
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi",  "Kwame"];
+    var femaleNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var male = document.getElementById("radioDefault1");
+    var female = document.getElementById("radioDefault2");
+
+function dayName() {
+  var birthYear = parseInt(document.getElementById("birthYear").value);
+  // if(birthYear.length !== 4) {
+      // alert("Year characters more than 4");
+  // }
+
+   var month = parseInt(document.getElementById("month").value);
+   
+   if(month >12 || month <= 0 ) {
+       alert("Enter a valid month");
+   }
+   
+   var day = parseInt(document.getElementById("date").value);
+
+   if(day <=0 || day > 31) {
+       alert("Enter a valid date");
+   }
+   
+   var dayNumber = new Date (birthYear,month-1,day).getDay();
+   console.log(dayNumber);
+
+   
+
+   if(male.checked) {
+       alert("Congratulations for knowing your Akan Name is: " + maleNames[dayNumber]);
+   } else if(female.checked){
+       alert("Congratulations  for knowing your Akan Name is: " + femaleNames[dayNumber])
+   } else {
+       alert("Kindly select your Gender and fill up the empty spaces:");
+   }
+   
+
+}
+
